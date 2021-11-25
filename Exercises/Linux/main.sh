debde1096ff63edb5730bd21dd792f5d892596e0
ang8210 <m22715286@gmail.com>
2021-11-25 09:26:29 +0800

    Modified exercises for linux

--- Exercises/Linux/main.sh
+++ Exercises/Linux/main.sh
@@ -4,8 +4,10 @@ pwd
 # (2) Check whether the file (hello.sh) exists in this directory
 #     - True： do (3)
 #     - False： Print out "No file!" on the terminal
-if [ -e 'hello.sh' ];then
-	./hello.sh
+if [ -f ./'hello.sh' ];then
+	while read line ;do
+		echo "$line"
+	done < hello.sh
 else
 	echo "NO file!"
 fi
@@ -15,7 +17,7 @@ fi
 # (4) In order to execute the file (hello.sh) to change the file permission
 #     - change the the file permission
 #     - execute the file
-chmod 777 hello.sh
+chmod +x hello.sh
 ./hello.sh
 
 # (5) Create a new directory name is Byebye and move hello.sh into Byebye
